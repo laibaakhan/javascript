59814c568be60d934b3fd8a7ba25954dfa277d0a
laibaakramkhan <ab1283841@gmail.com>
2023-04-08 01:06:18 +0500

    first commit

---
+++ chap#01/assigment01/app.js
@@ -0,0 +1,15 @@
+//------------------CHAPTER:01-------------
+                //QUESTION:01
+ // alert("Hello user ! wellcome to the webiste");
+                //QUESTION:02
+// alert("Error! please enter a valid password");
+                //QUESTION:03
+// alert("wellcome to JS land ...\n happy coding!");
+                //QUESTION:04
+ //alert("wellcome to JS land..");
+// alert("happy coding!");
+                //QUESTION:05 
+// alert("hello...I can run JS through my web brower's console") ;
+
+
+
---
+++ chap#02/assignment02/app.js
@@ -0,0 +1,47 @@
+// QUESTION NO: 01
+
+let username;
+
+// QUESTION NO: 02
+
+let myName = ("LAIBA AKRAM");
+
+//QUESTION NO: 03 
+
+let titledMessage;
+ titledMessage = ("Hello World");
+alert("hello world");
+
+// QUESTION NO: 04 
+
+let fullName = ("fatima");
+let studentsAge = ("18");
+ let studentsRollNo = ("WMA-103747");
+
+alert("fullName");
+alert("studentsAge");
+alert("studentsRollNo");
+
+// QUESTION NO: 05
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+
+// QUESTION NO: 06 
+
+let myEmail = ("ab1283841@gmail.com");
+alert("My Email address is " + "ab1283841@gmail.com");
+
+// QUESTION NO: 07
+
+let theBook = ("A smarter way to learn JavaScript");
+alert("I am trying to learn from the book A smarter \n way to learn JavaScript");
+
+//Question no:08
+document.write("YAh! I can write HTML Through javascript");
+
+// QUESTION NO: 09
+
+alert("▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ஜ۩۞۩ஜ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬")
+
+
